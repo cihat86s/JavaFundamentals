5a7e88bb2690838fcb47d94c75ea7a2cce06a68c
deepar <ccihat86s@hotmail.com>
2017-02-11 18:18:57 +0300

    first commit from windows

--- src/InheritanceAndComposition/Interfaces/HelloWorld.java
+++ src/InheritanceAndComposition/Interfaces/HelloWorld.java
@@ -1,11 +1,5 @@
 package InheritanceAndComposition.Interfaces;
 
-import InheritanceAndComposition.Inheritance.Machine;
-import InheritanceAndComposition.Inheritance.Printer;
-
-/**
- * Created by holywar on 10/02/2017.
- */
 public class HelloWorld {
     public static void main(String[] args) {
         IMachine machine = new InheritanceAndComposition.Interfaces.Printer(true, "MY PRINTER");
